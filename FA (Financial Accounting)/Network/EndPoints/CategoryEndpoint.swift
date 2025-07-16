d1edc2e8951075b8457f6a518133b0a6e454bb5e
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-16 22:26:36 +0300

    CategoriesEndpoint

---
+++ FA (Financial Accounting)/Network/EndPoints/CategoryEndpoint.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryEndpoint.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 16.07.2025.
+//
+
+import Foundation
+
+enum CategoryEndpoint {
+    case all
+    case type(direction: Direction)
+}
+
+extension CategoryEndpoint: Endpoint {
+    var baseURL: String {
+        return "https://shmr-finance.ru/api/v1/categories"
+    }
+    
+    var path: String {
+        switch self {
+            
+        case .all:
+            return ""
+        case .type(let direction):
+            return "/type/\(direction.rawValue)"
+        }
+    }
+    
+    var method: EndpointType {
+        return .get
+    }
+    
+    var queryItems: [URLQueryItem] {
+        return []
+    }
+    
+    
+}
