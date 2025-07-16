6c2ed8043a0979769ef24357bac0ac9f41f56381
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-17 02:10:02 +0300

    NetworkClient

---
+++ FA (Financial Accounting)/Network/NetworkClient.swift
@@ -0,0 +1,77 @@
+import Foundation
+
+struct NetworkClient {
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
+    
+    init(encoder: JSONEncoder, decoder: JSONDecoder) {
+        self.encoder = encoder
+        self.decoder = decoder
+    }
+    
+    func request<RequestBody: Encodable, ResponseBody: Decodable>(
+        body: RequestBody? = nil,
+        endpoint: Endpoint
+    ) async throws -> ResponseBody {
+        guard var components = URLComponents(url: endpoint.baseURL, resolvingAgainstBaseURL: false) else {
+            throw NetworkClientErrors.malformedURL
+        }
+        components.path = endpoint.path
+        components.queryItems = endpoint.queryItems
+        
+        guard let url = components.url else {
+            throw NetworkClientErrors.incorrectURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        if endpoint.authorized {
+            request.setValue("Bearer \(Constants.token)", forHTTPHeaderField: "Authorization")
+        }
+        
+        if let body = body {
+            do {
+                request.httpBody = try await Task.detached(priority: .background) {
+                    try encoder.encode(body)
+                }.value
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            } catch {
+                throw SerializationErrors.encodingError
+            }
+        }
+        
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkClientErrors.requestError
+        }
+        
+        switch httpResponse.statusCode {
+        case 200..<300:
+            do {
+                return try await Task.detached(priority: .background) {
+                    try decoder.decode(ResponseBody.self, from: data)
+                }.value
+            } catch {
+                throw SerializationErrors.decodingError
+            }
+        case 400:
+            throw APIError.badRequest(data)
+        case 401:
+            throw APIError.unauthorized
+        case 403:
+            throw APIError.forbidden
+        case 404:
+            throw APIError.notFound
+        case 409:
+            throw APIError.conflict
+        case 422:
+            throw APIError.unprocessableEntity(data)
+        case 500..<600:
+            throw APIError.serverError(code: httpResponse.statusCode)
+        default:
+            throw APIError.unexpectedStatusCode(response)
+        }
+    }
+}
---
+++ FA (Financial Accounting)/Network/Utils/NetworkClientErrors.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkClientErrors.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 16.07.2025.
+//
+import Foundation
+
+enum NetworkClientErrors: Error {
+    case malformedURL
+    case incorrectURL
+    case requestError
+}
+
+enum APIError: Error {
+    case nonHTTPResponse
+    case unexpectedStatusCode(URLResponse)
+    case badRequest(Data?)
+    case unauthorized
+    case forbidden
+    case notFound
+    case conflict
+    case unprocessableEntity(Data?)
+    case serverError(code: Int)
+    case decodingError(Error)
+}
+
+enum SerializationErrors: Error {
+    case encodingError
+    case decodingError
+}
+
