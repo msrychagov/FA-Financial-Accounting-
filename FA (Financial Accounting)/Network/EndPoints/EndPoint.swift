1b8ceda8f952a3f2ec0eeb129feefbe6ec3cf6e6
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-16 21:45:22 +0300

    Endpoint protocol

---
+++ FA (Financial Accounting)/Network/EndPoints/EndPoint.swift
@@ -0,0 +1,22 @@
+//
+//  EndPoint.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 16.07.2025.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: EndpointType { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+enum EndpointType: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
