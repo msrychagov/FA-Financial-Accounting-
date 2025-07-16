d68542ddb7ade79b662ed2851f92e26868a1fccd
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-16 22:18:54 +0300

    Accounts endpoints

---
+++ FA (Financial Accounting)/Network/EndPoints/AccountEndpoint.swift
@@ -0,0 +1,55 @@
+//
+//  AccountEndpoints.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 16.07.2025.
+//
+import Foundation
+
+enum AccountEndpoint {
+    case list
+    case single(id: String)
+    case create
+    case put(id: String)
+    case delete(id: String)
+}
+
+
+extension AccountEndpoint: Endpoint {
+    
+    var baseURL: String {
+        return "https://shmr-finance.ru/api/v1/accounts/"
+    }
+    
+    var path: String {
+        switch self {
+            
+        case .list, .create:
+            return ""
+        case .single(id: let id), .put(id: let id), .delete(id: let id):
+            return id
+        }
+    }
+    
+    var method: EndpointType {
+        switch self {
+            
+        case .list:
+                .get
+        case .single(id: let id):
+                .get
+        case .create:
+                .post
+        case .put(id: let id):
+                .put
+        case .delete(id: let id):
+                .delete
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        return []
+    }
+    
+    
+}
