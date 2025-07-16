f5636f403d9d53f78128c289fdb26dfd08e92a4d
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-16 21:57:24 +0300

    Fix transaction endpoints

--- FA (Financial Accounting)/Network/EndPoints/TransactionEndpoints.swift
+++ FA (Financial Accounting)/Network/EndPoints/TransactionEndpoints.swift
@@ -17,13 +17,13 @@ extension TransactionEndpoints: Endpoint {
     
     
     var baseURL: String {
-        return "https://shmr-finance.ru/api/v1/transactions"
+        return "https://shmr-finance.ru/api/v1/transactions/"
     }
     
     var path: String {
         switch self {
         case .list(accountId: let accountId, startDate: let startDate, endDate: let endDate):
-            return "transactions/account/\(accountId)"
+            return "account/\(accountId)/period"
         case .create:
             return ""
         case .put(let id), .delete(let id):
