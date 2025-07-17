e48670a34e94a1b54c9ab40a8f4a854c05530e1a
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-17 17:33:24 +0300

    DTO

--- FA (Financial Accounting)/BuisnessLogic/Services/Transactions/TransactionsService.swift
+++ FA (Financial Accounting)/BuisnessLogic/Services/Transactions/TransactionsService.swift
@@ -8,10 +8,10 @@ final class TransactionsService {
     }
     
     func fetchTransactions(
-        accountId: String,
+        accountId: Int,
         from startDate: Date = Date.startBorder,
         to endDate: Date = Date.endBorder
-    ) async throws -> [Transaction] {
+    ) async throws -> [TransactionDTO] {
         return try await networkClient.request(
             endpoint: TransactionEndpoints.list(
                 accountId: accountId,
@@ -27,7 +27,7 @@ final class TransactionsService {
         amount: Decimal,
         transactionDate: Date,
         comment: String
-    ) async throws -> Transaction {
+    ) async throws -> TransactionDTO {
         let endpoint = TransactionEndpoints.create
         let toCreateTransaction = CreateTransactionModel(
             accountId: accountId,
@@ -37,27 +37,27 @@ final class TransactionsService {
             comment: comment
         )
         
-        let newTransaction: Transaction = try await networkClient.request(body: toCreateTransaction, endpoint: endpoint)
+        let newTransaction: TransactionDTO = try await networkClient.request(body: toCreateTransaction, endpoint: endpoint)
         return newTransaction
     }
     
     //TODO: - Проверять, что транзакция с таким id существует
-    func fetchTransactionDetails(id: Int) async throws -> Transaction {
+    func fetchTransactionDetails(id: Int) async throws -> TransactionDTO {
         let endpoint = TransactionEndpoints.details(id: id)
-        let transaction: Transaction = try await networkClient.request(endpoint: endpoint)
+        let transaction: TransactionDTO = try await networkClient.request(endpoint: endpoint)
         return transaction
     }
     
-    func updateTransaction(id: Int, accountId: Int, categoryId: Int, amount: Decimal, transactionDate: Date, comment: String) async throws -> Transaction {
+    func updateTransaction(id: Int, accountId: Int, categoryId: Int, amount: Decimal, transactionDate: Date, comment: String) async throws -> TransactionDTO {
         let endpoint = TransactionEndpoints.put(id: id)
         let toUpdateTransaction = UpdateTransactionModel(accountId: accountId, categoryId: categoryId, amount: amount, transactionDate: transactionDate, comment: comment)
-        let updatedTransaction: Transaction = try await networkClient.request(body: toUpdateTransaction, endpoint: endpoint)
+        let updatedTransaction: TransactionDTO = try await networkClient.request(body: toUpdateTransaction, endpoint: endpoint)
         return updatedTransaction
     }
     
-    func deleteTransaction(id: Int) async throws -> Transaction {
+    func deleteTransaction(id: Int) async throws -> TransactionDTO {
         let endpoint = TransactionEndpoints.delete(id: id)
-        let deletedTransaction: Transaction = try await networkClient.request(endpoint: endpoint)
+        let deletedTransaction: TransactionDTO = try await networkClient.request(endpoint: endpoint)
         return deletedTransaction
     }
 }
