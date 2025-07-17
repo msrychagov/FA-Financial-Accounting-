d3830531484f24e520714bab0b758b3778a4c0e3
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-17 17:40:32 +0300

    Extensions

---
+++ FA (Financial Accounting)/BuisnessLogic/Models/Category/Extensions/Category+JSON.swift
@@ -0,0 +1,39 @@
+//
+//  Category+JSON.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 11.06.2025.
+//
+
+import Foundation
+import CoreData
+
+extension Category {
+    static func parse(jsonObject: Any) -> Category? {
+        guard let dict = jsonObject as? [String: Any] else { return nil }
+        
+        guard let emoji = dict["emoji"] as? String,
+              let name = dict["name"] as? String,
+              let idString = dict["id"] as? String,
+              let id = Int(idString),
+              let isIncome = dict["isIncome"] as? Bool
+        else { return nil }
+
+        let cat = Category(id: id, name: name, emoji: emoji, isIncome: isIncome)
+        
+        return cat
+              
+    }
+    
+    var jsonObject: Any {
+        let dict: [String: Any] = [
+            "emoji" : emoji,
+            "name" : name,
+            "id" : String(id),
+            "isIncome" : isIncome
+        ]
+        return dict
+    }
+}
+
+
