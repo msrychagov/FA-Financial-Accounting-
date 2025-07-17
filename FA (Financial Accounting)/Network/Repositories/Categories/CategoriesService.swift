73f53d88ac8ef11fc2acf6739c39312dbe25a047
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-07-18 02:02:04 +0300

    CategoriesService

--- FA (Financial Accounting)/Network/Repositories/Categories/CategoriesService.swift
+++ FA (Financial Accounting)/Network/Repositories/Categories/CategoriesService.swift
@@ -5,6 +5,18 @@
 //  Created by Михаил Рычагов on 17.07.2025.
 //
 
-//final class CategoriesService {
-//    func all() -> 
-//}
+final class CategoriesService {
+    private let repo: CategoriesRepository
+    
+    init(repo: CategoriesRepository = CategoriesRepository()) {
+        self.repo = repo
+    }
+    
+    func fetchAll() async throws -> [Category] {
+        try await repo.fetchAll()
+    }
+    
+    func fetchTypeList(direction: Direction) async throws -> [Category] {
+        try await repo.fetchTypeList(direction: direction)
+    }
+}
