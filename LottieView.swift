074c5b5cfa8a5d84001864545c4b6ad23e037775
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-08-15 23:23:37 +0300

    fix: packages-deps

---
+++ LottieView.swift
@@ -0,0 +1,33 @@
+//
+//  LaunchScreen.swift
+//  FA (Financial Accounting)
+//
+//  Created by Михаил Рычагов on 24.07.2025.
+//
+
+import SwiftUI
+import Lottie
+
+public struct LottieView: UIViewRepresentable {
+    private let name: String
+    private let onComplete: () -> Void
+
+    public init(name: String, onComplete: @escaping () -> Void) {
+        self.name = name
+        self.onComplete = onComplete
+    }
+
+    public func makeUIView(context: Context) -> LottieAnimationView {
+        let v = LottieAnimationView()
+        let bundle = Bundle.module
+
+        v.animation = LottieAnimation.named(name, bundle: bundle)
+        v.contentMode = .scaleAspectFit
+        v.loopMode = .playOnce
+        v.play { finished in if finished { onComplete() } }
+        return v
+    }
+
+    public func updateUIView(_ uiView: LottieAnimationView, context: Context) {}
+}
+
